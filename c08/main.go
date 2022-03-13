ebfe62f9cad77741234b23c95c29ae09ab4b6f2a
juanpickselov <juanpickselov@sbcglobal.net>
2022-03-13 16:34:45 -0400

    struct and types sample for chapter 8

--- c08/main.go
+++ c08/main.go
@@ -25,4 +25,14 @@ func main() {
 	fmt.Println("City:", employee.City)
 	fmt.Println("State:", employee.State)
 	fmt.Println("Postal Code:", employee.PostalCode)
+
+	// NOTE below you can use the longer variable dot Address dot street form but variable dot Street is much less typing :)
+	employee01 := magazine.Employee{Name: "Juan Pickselov", Salary: 87999}
+	employee01.Address.Street = "123 Lotus Lane"
+	employee01.City = "Battlefront"
+	employee01.State = "CO"
+	fmt.Println("Street:", employee01.Street)
+	fmt.Println("City:", employee01.City)
+	fmt.Println("State:", employee01.State)
+	fmt.Println("Postal Code:", employee01.PostalCode)
 }
